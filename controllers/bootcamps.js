ebe45fd50723e65d6473f6cf5285409cfe705be8
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-01-25 16:07:35 +0400

    adding controllers

---
+++ controllers/bootcamps.js
@@ -0,0 +1,40 @@
+// @desc getAll Bootcamps
+// @route GET /api/v1/bootcamps
+// @access Public
+
+exports.getBootcamps=(req,res)=>{
+    res.status(200).json({success:true,msg:"Get All bootcamps"});
+}
+
+// @desc get Single Bootcamp
+// @route GET /api/v1/bootcamp/:id
+// @access Public
+
+exports.getBootcamp=(req,res)=>{
+    res.status(200).json({success:true,msg:`Get single bootcamp with id ${req.params.id}`});
+    
+}
+
+// @desc create bootcamp
+// @route POST /api/v1/bootcamps
+// @access Private
+
+exports.createBootcamp=(req,res)=>{
+    res.status(201).json({success:true,msg:"create bootcamp"});
+}
+
+// @desc update bootcamp
+// @route PUT /api/v1/bootcamps/:id
+// @access Private
+
+exports.updateBootcamp=(req,res)=>{
+    res.status(200).json({success:true,msg:`Update single bootcamp with id ${req.params.id}`});
+}
+
+// @desc delete bootcamp
+// @route DELETE /api/v1/bootcamps/:id
+// @access Private
+
+exports.deleteBootcamp=(req,res)=>{
+    res.status(200).json({success:true,msg:`Delete single bootcamp with id ${req.params.id}`});
+}
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,21 +1,20 @@
-const express=require("express")
+const express = require('express');
 
-const router=express.Router();
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require('../controllers/bootcamps');
 
-router.get("/",(req,res)=>{
-    res.status(200).json({success:true,msg:"Get All bootcamps"});
-})
-router.post("/",(req,res)=>{
-    res.status(200).json({success:true,msg:"create bootcamp"});
-})
-router.get("/:id",(req,res)=>{
-    res.status(200).json({success:true,msg:`Get single bootcamp with id ${req.params.id}`});
-})
-router.put("/:id",(req,res)=>{
-    res.status(200).json({success:true,msg:`Update single bootcamp with id ${req.params.id}`});
-})
-router.delete("/:id",(req,res)=>{
-    res.status(200).json({success:true,msg:`Delete single bootcamp with id ${req.params.id}`});
-})
+const router = express.Router();
 
-module.exports=router;
+router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+module.exports = router;
