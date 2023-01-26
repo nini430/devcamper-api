502aa2166c3b86aee77bb4271872cd763ac034fe
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-01-26 12:23:18 +0400

    add mongoose connection

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express=require("express")
 const dotenv=require("dotenv")
 const morgan=require("morgan");
+const colors=require("colors");
+const connectDB=require("./config/db")
 
 // routes
 
 const bootcamps=require("./routes/bootcamps")
 
 
+
 dotenv.config({path:"./config/config.env"});
+
+
+connectDB();
 
 const app=express();
 
@@ -18,6 +24,11 @@ app.use("/api/v1/bootcamps",bootcamps);
 
 const PORT=process.env.PORT||7000
 
-app.listen(PORT,()=>{
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server=app.listen(PORT,()=>{
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+})
+
+process.on("unhandledRejection",(err,promise)=>{
+    console.log(`Error ${err.message}`.red);
+    server.close(()=>process.exit(1));
 })
